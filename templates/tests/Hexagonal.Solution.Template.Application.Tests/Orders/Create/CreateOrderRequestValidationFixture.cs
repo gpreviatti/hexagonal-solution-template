4dd7c90599155a276f0e8757f214ade6b8d46692
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2023-12-22 07:45:45 -0300

    add attribute and create base tests

---
+++ templates/tests/Hexagonal.Solution.Template.Application.Tests/Orders/Create/CreateOrderRequestValidationFixture.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FluentValidation.TestHelper;
+using Hexagonal.Solution.Template.Application.Orders.Create;
+using Hexagonal.Solution.Template.Tests.Common.Attributes;
+
+namespace Hexagonal.Solution.Template.Application.Tests.Orders.Create;
+public class CreateOrderRequestValidationFixture
+{
+    public IValidator<CreateOrderRequest> validator;
+
+    public CreateOrderRequestValidationFixture()
+    {
+        validator = new CreateOrderRequestValidator();
+    }
+}
---
+++ templates/tests/Hexagonal.Solution.Template.Application.Tests/Orders/Create/CreateOrderRequestValidationTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.TestHelper;
+using Hexagonal.Solution.Template.Application.Orders.Create;
+using Hexagonal.Solution.Template.Tests.Common.Attributes;
+
+namespace Hexagonal.Solution.Template.Application.Tests.Orders.Create;
+public class CreateOrderRequestValidationTests(CreateOrderRequestValidationFixture fixture) : IClassFixture<CreateOrderRequestValidationFixture>
+{
+    private readonly CreateOrderRequestValidationFixture _fixture = fixture;
+
+    [Theory(DisplayName = nameof(Given_A_Valid_Request_Then_Pass))]
+    [JsonResourceData("create-order-request-tests.json", "valid")]
+    public async Task Given_A_Valid_Request_Then_Pass(CreateOrderRequest request)
+    {
+        // Arrange, Act
+        var result = await _fixture.validator.TestValidateAsync(request);
+
+        // Assert
+        result.ShouldHaveAnyValidationError();
+    }
+
+    [Theory(DisplayName = nameof(Given_A_Invalid_Request_Then_Fails))]
+    [JsonResourceData("create-order-request-tests.json", "invalid")]
+    public async Task Given_A_Invalid_Request_Then_Fails(CreateOrderRequest request, string propertyName)
+    {
+        // Arrange, Act
+        var result = await _fixture.validator.TestValidateAsync(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(propertyName);
+    }
+}
