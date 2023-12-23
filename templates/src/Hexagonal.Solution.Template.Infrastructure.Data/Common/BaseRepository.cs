4d1ecd70af2fcd9cbb0eef7a3c80d6c0ea8dc57f
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2023-12-23 09:22:14 -0300

    add base repository

---
+++ templates/src/Hexagonal.Solution.Template.Infrastructure.Data/Common/BaseRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Hexagonal.Solution.Template.Infrastructure.Data.Common;
+public abstract class BaseRepository<T> : IBaseRepository<T> where T : class, new()
+{
+    protected readonly DbContext dbContext;
+    protected readonly DbSet<T> dbEntitySet;
+
+    public BaseRepository(DbContext dbContext)
+    {
+        this.dbContext = dbContext;
+        dbEntitySet = this.dbContext.Set<T>();
+    }
+
+    public async Task<T> Add(T entity)
+    {
+        await dbEntitySet.AddAsync(entity);
+        return entity;
+    }
+
+    public async Task AddRange(T[] entities) =>
+        await dbEntitySet.AddRangeAsync(entities);
+
+    public void BeginTransaction()
+    {
+        if (dbContext.Database.CurrentTransaction == null)
+            dbContext.Database.BeginTransaction();
+    }
+
+    public void CommitTransaction()
+    {
+        if (dbContext.Database.CurrentTransaction != null)
+        {
+            dbContext.SaveChanges();
+            dbContext.Database.CurrentTransaction.Commit();
+        }
+    }
+
+    public void RollbackTransaction()
+    {
+        if (dbContext.Database.CurrentTransaction != null)
+            dbContext.Database.CurrentTransaction.Rollback();
+    }
+
+    public async Task<bool> CheckExistsByWhere(Expression<Func<T, bool>> predicate)
+        => await dbEntitySet.AnyAsync(predicate);
+
+    public async Task<bool> CheckExistsByWhereAsNoTracking(Expression<Func<T, bool>> predicate)
+        => await dbEntitySet.AsNoTracking().AnyAsync(predicate);
+
+    public async Task<T?> GetById(object id) => await dbEntitySet.FindAsync(id);
+
+    public async Task<IList<T>> GetByWhere(Expression<Func<T, bool>> predicate)
+        => await dbEntitySet.Where(predicate).ToListAsync();
+
+    public async Task<IList<T>> GetByWhereAsNoTracking(Expression<Func<T, bool>> predicate)
+        => await dbEntitySet.AsNoTracking().Where(predicate).ToListAsync();
+
+    public async Task<T?> FirstOrDefaultAsNoTracking(Expression<Func<T, bool>> predicate)
+        => await dbEntitySet.AsNoTracking().FirstOrDefaultAsync(predicate);
+
+    public void Update(T entity) => dbEntitySet.Update(entity);
+
+    public void Remove(T entity) => dbEntitySet.Remove(entity);
+
+    public void RemoveRange(T[] entities) => dbEntitySet.RemoveRange(entities);
+
+    public async Task<T> AddOrUpdateIfNotExistsAsync(T entity, Expression<Func<T, bool>> predicate)
+    {
+        var exists = dbEntitySet.AsNoTracking().Any(predicate);
+
+        if (!exists)
+            await dbEntitySet.AddAsync(entity);
+        else
+            dbEntitySet.Update(entity);
+
+        return entity;
+    }
+}
---
+++ templates/src/Hexagonal.Solution.Template.Infrastructure.Data/Common/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace Hexagonal.Solution.Template.Infrastructure.Data.Common;
+public interface IBaseRepository<T> where T : class, new()
+{
+    Task<T> Add(T entity);
+    Task<T> AddOrUpdateIfNotExistsAsync(T entity, Expression<Func<T, bool>> predicate);
+    Task AddRange(T[] entities);
+    void BeginTransaction();
+    Task<bool> CheckExistsByWhere(Expression<Func<T, bool>> predicate);
+    Task<bool> CheckExistsByWhereAsNoTracking(Expression<Func<T, bool>> predicate);
+    void CommitTransaction();
+    Task<T?> FirstOrDefaultAsNoTracking(Expression<Func<T, bool>> predicate);
+    Task<T?> GetById(object id);
+    Task<IList<T>> GetByWhere(Expression<Func<T, bool>> predicate);
+    Task<IList<T>> GetByWhereAsNoTracking(Expression<Func<T, bool>> predicate);
+    void Remove(T entity);
+    void RemoveRange(T[] entities);
+    void RollbackTransaction();
+    void Update(T entity);
+}
