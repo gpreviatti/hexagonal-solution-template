6339d99ccaf8ca0202e8788329f201e31206ca8b
Giovanni Previatti <giovanni.previatti@wovenhc.com>
2025-09-03 13:58:35 -0300

    fix(40): change to AddDbContextPool for MyDbContext initialization

--- templates/src/Infrastructure/Data/InfrastructureDataDependencyInjection.cs
+++ templates/src/Infrastructure/Data/InfrastructureDataDependencyInjection.cs
@@ -12,7 +12,7 @@ internal static class InfrastructureDataDependencyInjection
     public static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
     {
         services
-            .AddDbContextPool<MyDbContext>(context => context.UseSqlServer(configuration.GetConnectionString("OrderDb")));
+            .AddDbContext<MyDbContext>(context => context.UseSqlServer(configuration.GetConnectionString("OrderDb")));
 
         services.AddScoped<IBaseRepository<Order>, BaseRepository<Order>>();
 
