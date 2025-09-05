8a55852e8ebe2a5d00c3614fc0fd44582ba42ad2
Giovanni Previatti <giovanni.previatti@wovenhc.com>
2025-09-05 08:14:21 -0300

    feat(43): add seq service to docker-compose and update otel settings

--- templates/src/Infrastructure/OpenTelemetry/InfrastructureOpenTelemetryDependencyInjection.cs
+++ templates/src/Infrastructure/OpenTelemetry/InfrastructureOpenTelemetryDependencyInjection.cs
@@ -2,6 +2,7 @@ using Application.Common.Constants;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -13,13 +14,21 @@ internal static class InfrastructureOpenTelemetryDependencyInjection
 {
     public static WebApplicationBuilder AddOpenTelemetry(this WebApplicationBuilder builder)
     {
+        var exporterProtocol = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_PROTOCOL")?.ToLower() == "grpc"
+            ? OtlpExportProtocol.Grpc
+            : OtlpExportProtocol.HttpProtobuf;
+
         builder.Services.AddOpenTelemetry()
         .ConfigureResource(resource => resource.AddEnvironmentVariableDetector())
         .WithMetrics(metrics => metrics
             .AddAspNetCoreInstrumentation()
             .AddMeter(DefaultConfigurations.Meter.Name)
             .AddHttpClientInstrumentation()
-            .AddOtlpExporter()
+            .AddOtlpExporter(options =>
+            {
+                options.Protocol = exporterProtocol;
+                options.Endpoint = new Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_METRICS_ENDPOINT")!);
+            })
         )
         .WithTracing(tracing => tracing
             .AddAspNetCoreInstrumentation()
@@ -38,12 +47,24 @@ internal static class InfrastructureOpenTelemetryDependencyInjection
             )
             .AddRedisInstrumentation()
             .AddConsoleExporter()
-            .AddOtlpExporter()
-        )
-        .WithLogging(logging => logging
-            .AddConsoleExporter()
-            .AddOtlpExporter());
-        
+            .AddOtlpExporter(options =>
+            {
+                options.Protocol = exporterProtocol;
+                options.Endpoint = new Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT")!);
+            })
+        );
+
+        builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLoggerOptions =>
+        {
+            openTelemetryLoggerOptions.IncludeScopes = true;
+            openTelemetryLoggerOptions.IncludeFormattedMessage = true;
+            openTelemetryLoggerOptions.AddOtlpExporter(options =>
+            {
+                options.Protocol = exporterProtocol;
+                options.Endpoint = new Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_LOGS_ENDPOINT")!);
+            });
+        }));
+
         return builder;
     }
 }
