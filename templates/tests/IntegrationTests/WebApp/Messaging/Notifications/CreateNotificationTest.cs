829378e826e8e1fd539f571ad0604263fecb6b2f
Giovanni Previatti <giovanni.previatti@wovenhc.com>
2025-11-10 11:06:37 -0300

    feat: add CreateNotificationTest and fixture for notification testing

---
+++ templates/tests/IntegrationTests/WebApp/Messaging/Notifications/CreateNotificationTest.cs
@@ -0,0 +1,14 @@
+using CommonTests.Fixtures;
+using IntegrationTests.Common;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace IntegrationTests.WebApp.Messaging.Notifications;
+
+public class CreateNotificationTestFixture : BaseFixture
+{
+}
+
+public sealed class CreateNotificationTest(CustomWebApplicationFactory<Program> customWebApplicationFactory) : CreateNotificationTestFixture(customWebApplicationFactory)
+{
+
+}
