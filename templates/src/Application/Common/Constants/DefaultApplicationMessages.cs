204a201bc9e1ffc4bb1a7ea46897c402dbecc30c
Giovanni B. Previatti <giovanni.previatti@outlook.com>
2025-12-21 15:58:55 -0300

    feat: remove an extra whitespace and improve logging messages

--- templates/src/Application/Common/Constants/DefaultApplicationMessages.cs
+++ templates/src/Application/Common/Constants/DefaultApplicationMessages.cs
@@ -3,6 +3,6 @@ namespace Application.Common.Constants;
 public static class DefaultApplicationMessages
 {
     public const string StartToExecuteUseCase = "[{ClassName}] | [{MethodName}] | [{CorrelationId}] | Start to execute use case";
-    public const string FinishedExecutingUseCase = "[{ClassName}] | [{MethodName}] | [{CorrelationId}] |  Elapsed time: {ElapsedMilliseconds} ms | Finished executing use case";
+    public const string FinishedExecutingUseCase = "[{ClassName}] | [{MethodName}] | [{CorrelationId}] | Elapsed time: {ElapsedMilliseconds} ms | Finished executing use case";
     public const string ValidationErrors = "[{ClassName}] | [{MethodName}] | [{CorrelationId}] | Validation errors: [{errors}]";
 }
