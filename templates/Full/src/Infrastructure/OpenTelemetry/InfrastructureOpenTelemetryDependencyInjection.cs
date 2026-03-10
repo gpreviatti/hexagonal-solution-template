77bbd5834617753604e4e8d11d58c1a9046a12a9
Giovanni Previatti <giovanni.previatti@wovenhc.com>
2026-03-10 07:23:31 -0300

    53 feat: update docker-compose and configuration for observability

--- templates/Full/src/Infrastructure/OpenTelemetry/InfrastructureOpenTelemetryDependencyInjection.cs
+++ templates/Full/src/Infrastructure/OpenTelemetry/InfrastructureOpenTelemetryDependencyInjection.cs
@@ -39,13 +39,6 @@ internal static class InfrastructureOpenTelemetryDependencyInjection
             .ConfigureResource(resource => resource.AddEnvironmentVariableDetector())
             .WithMetrics(metrics =>
             {
-                metrics.AddView(
-                    "http.server.request.duration",
-                    new ExplicitBucketHistogramConfiguration()
-                    {
-                        Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
-                    }
-                );
                 metrics.AddMeter(
                     DefaultConfigurations.Meter.Name,
                     "System.Diagnostics.Metrics",
@@ -85,7 +78,6 @@ internal static class InfrastructureOpenTelemetryDependencyInjection
                 })
             )
             .WithLogging(logging => logging
-                .AddConsoleExporter()
                 .AddOtlpExporter(options =>
                 {
                     options.Protocol = exporterProtocol;
