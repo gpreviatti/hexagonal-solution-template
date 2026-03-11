c9f05c83a5470c9da12f52fa01ff58054cc36ece
Giovanni Previatti <giovanni.previatti@wovenhc.com>
2026-03-11 07:43:56 -0300

    53 refactor: simplify open telemetry configuration and logging

--- templates/Full/src/Infrastructure/OpenTelemetry/InfrastructureOpenTelemetryDependencyInjection.cs
+++ templates/Full/src/Infrastructure/OpenTelemetry/InfrastructureOpenTelemetryDependencyInjection.cs
@@ -3,8 +3,6 @@ using Grafana.OpenTelemetry;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using OpenTelemetry.Exporter;
-using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -17,79 +15,58 @@ internal static class InfrastructureOpenTelemetryDependencyInjection
     {
         public WebApplicationBuilder AddOpenTelemetry()
         {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var exporterProtocol = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_PROTOCOL")?.ToLower(System.Globalization.CultureInfo.InvariantCulture) == "grpc"
-                ? OtlpExportProtocol.Grpc
-                : OtlpExportProtocol.HttpProtobuf;
-            var exporterMetricsEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_METRICS_ENDPOINT");
-            var exporterTracesEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT");
-            var exporterLogsEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_LOGS_ENDPOINT");
-
-            if (
-                string.Equals(environment, "IntegrationTests", StringComparison.OrdinalIgnoreCase) ||
-                string.IsNullOrWhiteSpace(exporterLogsEndpoint) ||
-                string.IsNullOrWhiteSpace(exporterMetricsEndpoint) ||
-                string.IsNullOrWhiteSpace(exporterTracesEndpoint)
-            )
-            {
-                return builder;
-            }
+            var resourceBuilder = ResourceBuilder.CreateDefault()
+                .AddService("Hexagonal.Solution.Template.WebApp");
 
             builder.Services.AddOpenTelemetry()
-            .ConfigureResource(resource => resource.AddEnvironmentVariableDetector())
-            .WithMetrics(metrics =>
-            {
-                metrics.AddMeter(
-                    DefaultConfigurations.Meter.Name,
-                    "System.Diagnostics.Metrics",
-                    "Microsoft.AspNetCore.Hosting",
-                    "Microsoft.AspNetCore.Server.Kestrel",
-                    "System.Net.Http"
-                );
+                .WithMetrics(metrics =>
+                {
+                    metrics.AddView(
+                        "http.server.request.duration",
+                        new ExplicitBucketHistogramConfiguration()
+                        {
+                            Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
+                        }
+                    );
+                    metrics.AddMeter(
+                        DefaultConfigurations.Meter.Name,
+                        "System.Diagnostics.Metrics",
+                        "Microsoft.AspNetCore.Hosting",
+                        "Microsoft.AspNetCore.Server.Kestrel",
+                        "System.Net.Http"
+                    );
 
-                metrics
+                    metrics
+                        .SetResourceBuilder(resourceBuilder)
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddRuntimeInstrumentation()
+                        .AddProcessInstrumentation()
+                        .AddPrometheusExporter()
+                        .UseGrafana();
+                })
+                .WithTracing(tracing => tracing
+                    .AddSource("Hexagonal.Solution.Template.WebApp")
+                    .SetResourceBuilder(resourceBuilder)
+                    .AddRedisInstrumentation()
+                    .AddRabbitMQInstrumentation()
+                    .AddGrpcClientInstrumentation()
                     .AddAspNetCoreInstrumentation()
                     .AddHttpClientInstrumentation()
-                    .AddRuntimeInstrumentation()
-                    .AddProcessInstrumentation()
-                    .AddSqlClientInstrumentation()
-                    .AddPrometheusExporter()
-                    .AddOtlpExporter(options =>
-                    {
-                        options.Protocol = exporterProtocol;
-                        options.Endpoint = new Uri(exporterMetricsEndpoint);
-                    })
-                    .UseGrafana();
-            })
-            .WithTracing(tracing => tracing
-                .AddSqlClientInstrumentation()
-                .AddRedisInstrumentation()
-                .AddRabbitMQInstrumentation()
-                .AddGrpcClientInstrumentation()
-                .AddAspNetCoreInstrumentation()
-                .AddHttpClientInstrumentation()
-                .AddEntityFrameworkCoreInstrumentation()
-                .AddOtlpExporter(options =>
-                {
-                    options.Protocol = exporterProtocol;
-                    options.Endpoint = new Uri(exporterTracesEndpoint!);
-                })
-                .UseGrafana()
-            );
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .UseGrafana()
+                );
 
-            builder.Services.AddLogging(logging => logging.AddOpenTelemetry(options =>
+            builder.Logging.AddOpenTelemetry(logging =>
             {
-                options.IncludeFormattedMessage = true;
-                options.IncludeScopes = true;
-                options.ParseStateValues = true;
-                options
+                logging.SetResourceBuilder(resourceBuilder);
+                logging.IncludeFormattedMessage = true;
+                logging.IncludeScopes = true;
+                logging.ParseStateValues = true;
+                logging
                     .AttachLogsToActivityEvent()
-                    .AddOtlpExporter(exporterOptions =>
-                    {
-                        exporterOptions.Protocol = exporterProtocol;
-                        exporterOptions.Endpoint = new Uri(exporterLogsEndpoint!);
-                    }).UseGrafana();
-            }));
+                    .UseGrafana();
+            });
 
             return builder;
         }
