bf2e75a288e6867db7a0805069e22a199b96f6e6
Giovanni B. Previatti <gpreviatt@gmail.com>
2026-05-01 14:32:37 -0300

    feat(83): enhance request and response structures with detailed XML documentation

--- templates/WebUi/src/Contracts/Orders/GetOrderSummaryRequest.cs
+++ templates/WebUi/src/Contracts/Orders/GetOrderSummaryRequest.cs
@@ -2,4 +2,8 @@ using Contracts.Common;
 
 namespace Contracts.Orders;
 
+/// <summary>
+/// Represents the request for retrieving order summary information.
+/// </summary>
+/// <param name="CorrelationId"></param>
 public sealed record GetOrderSummaryRequest(Guid CorrelationId) : BaseRequest(CorrelationId);
