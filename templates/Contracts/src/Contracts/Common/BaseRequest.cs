de4af84b9116b5afbe8fac98da856b6bacce77fe
Giovanni Brunno Previatti <giovanni.previatti@wovenhc.com>
2026-07-30 09:04:07 -0300

    feat: improve request on contracts templates

--- templates/Contracts/src/Contracts/Common/BaseRequest.cs
+++ templates/Contracts/src/Contracts/Common/BaseRequest.cs
@@ -4,7 +4,9 @@
 /// Base request structure
 /// </summary>
 /// <param name="CorrelationId">The unique identifier for correlating requests</param>
-public record BaseRequest(Guid CorrelationId);
+/// <param name="User">The user making the request</param>
+/// <param name="TimezoneId">The timezone identifier for the request</param>
+public record BaseRequest(Guid CorrelationId, string User = "", string TimezoneId = "");
 
 /// <summary>
 /// Base paginated request structure
@@ -15,11 +17,37 @@ public record BaseRequest(Guid CorrelationId);
 /// <param name="SortBy">The field to sort by</param>
 /// <param name="SortDescending">Indicates whether the sorting is in descending order</param>
 /// <param name="SearchByValues">A dictionary of search criteria</param>
+/// <param name="User">The user making the request</param>
+/// <param name="TimezoneId">The timezone identifier for the request</param>
 public record BasePaginatedRequest(
     Guid CorrelationId,
     int Page = 1,
     int PageSize = 10,
     string? SortBy = null,
     bool SortDescending = false,
-    Dictionary<string, string>? SearchByValues = null
-) : BaseRequest(CorrelationId);
+    Dictionary<string, string>? SearchByValues = null,
+    string User = "",
+    string TimezoneId = ""
+) : BaseRequest(CorrelationId, User, TimezoneId);
+
+/// <summary>
+/// Represents a request for paginated data with full-text search capabilities.
+/// </summary>
+/// <param name="CorrelationId">The unique identifier for correlating requests</param>
+/// <param name="Page">The page number to retrieve</param>
+/// <param name="PageSize">The number of items per page</param>
+/// <param name="SortBy">The field to sort by</param>
+/// <param name="SortDescending">Indicates whether the sorting is in descending order</param>
+/// <param name="SearchValue">The value to search for within the full-text index e.g. "search term"</param>
+/// <param name="User">The user making the request</param>
+/// <param name="TimezoneId">The timezone identifier for the request</param>
+public record BaseFullTextSearchPaginatedRequest(
+    Guid CorrelationId,
+    int Page = 1,
+    int PageSize = 10,
+    string? SortBy = null,
+    bool SortDescending = false,
+    string SearchValue = "",
+    string User = "",
+    string TimezoneId = ""
+) : BaseRequest(CorrelationId, User, TimezoneId);
